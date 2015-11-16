27325c77dab26b98dced6f8c9b7f0bcf1a067400
Christopher Moore <moore.christopher515@gmail.com>
2015-11-16 12:55:17 -0600

    Create red_black_template.h

---
+++ Project-Two/red_black_template.h
@@ -0,0 +1,20 @@
+//
+// Created by Chris-PC on 11/16/2015.
+//
+
+#ifndef RED_BLACK_TREE_PROJECT_RED_BLACK_TEMPLATE_H_H
+#define RED_BLACK_TREE_PROJECT_RED_BLACK_TEMPLATE_H_H
+
+class tree
+{
+public:
+    tree();
+    bool empty();
+    bool search();
+    void insert();
+    void remove();
+    void graph();
+private:
+
+};
+#endif //RED_BLACK_TREE_PROJECT_RED_BLACK_TEMPLATE_H_H
