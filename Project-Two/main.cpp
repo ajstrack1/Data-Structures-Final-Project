9ec97e4d444f35a22a116161f1497fe702f9c11c
Christopher Moore <moore.christopher515@gmail.com>
2015-11-16 12:54:20 -0600

    Create main.cpp

---
+++ Project-Two/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Implement Red-Black Tree and its operations.
+ *
+ * Goals:
+ * Accept input from user
+ * Construct and display the tree
+ * Prompt for item to be deleted. Display tree after deletion
+ *
+ *
+ */
+
+#include <iostream>
+#include "red_black_template.h"
+using namespace std;
+
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
